06b1889a0abcabc913830b7f39f8c6808fbaa2c3
Furkan KAMACI <furkan.kamaci@amadeus.com>
2025-02-15 19:37:30 +0300

    category side eklendi

---
+++ src/app/pages/settings-page/category-component/category-component.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-category-component',
+  imports: [],
+  templateUrl: './category-component.component.html',
+  styleUrl: './category-component.component.scss'
+})
+export class CategoryComponentComponent {
+
+}
--- src/app/pages/settings-page/settings-page.component.ts
+++ src/app/pages/settings-page/settings-page.component.ts
@@ -1,15 +1,19 @@
 import { Component } from '@angular/core';
 import { SettingsSidebarComponent } from './settings-sidebar/settings-sidebar.component';
 import { CommonModule } from '@angular/common';
+import { CategoryComponentComponent } from './category-component/category-component.component';
 
 @Component({
   selector: 'app-settings-page',
-  imports: [SettingsSidebarComponent, CommonModule],
+  imports: [SettingsSidebarComponent, CommonModule,
+    CategoryComponentComponent
+  ],
   templateUrl: './settings-page.component.html',
   styleUrl: './settings-page.component.scss'
 })
 export class SettingsPageComponent {
-selectedSection: string = 'profile';
+  // selectedSection: string = 'profile';
+  selectedSection: string = 'category';
 
   onSectionChange(section: string) {
     this.selectedSection = section;
