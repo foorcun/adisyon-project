0481fc9fbe5e85770de2d03afb7227f419b30da8
Furkan KAMACI <furkan.kamaci@amadeus.com>
2025-02-15 19:26:37 +0300

    settings-page eklendi

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-    { path: "" , loadComponent: () => import('./common/hello-firebase/hello-firebase.component').then(m => m.HelloFirebaseComponent) }
+    { path: "", loadComponent: () => import('./common/hello-firebase/hello-firebase.component').then(m => m.HelloFirebaseComponent) },
+    { path: "settings-page", loadComponent: () => import('./pages/settings-page/settings-page.component').then(m => m.SettingsPageComponent) },
 ];
