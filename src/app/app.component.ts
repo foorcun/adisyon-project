9ed1dd04c48139ec4fb9f3d7917aa5f5cdfca168
Furkan KAMACI <furkan.kamaci@amadeus.com>
2025-03-02 17:45:46 +0300

    if this page:/table-details/... don't show bottom navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,31 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
 import { NavbarBootstrapComponent } from './common/navbar-bootstrap/navbar-bootstrap.component';
 import { BottomNavigationBarComponent } from './common/bottom-navigation-bar/bottom-navigation-bar.component';
+import { filter } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, BottomNavigationBarComponent ],
+  imports: [RouterOutlet, BottomNavigationBarComponent, CommonModule ],
   // imports: [RouterOutlet ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'adisyon-project';
+ title = 'adisyon-project';
+  showBottomNav = true;
+
+  constructor(private router: Router) {
+    this.router.events.pipe(filter(event => event instanceof NavigationEnd)).subscribe(() => {
+      this.checkRoute();
+    });
+  }
+
+  private checkRoute() {
+    const currentUrl = this.router.url;
+    const tableDetailsPattern = /^\/table-details\/[a-zA-Z0-9_-]+$/;
+
+    this.showBottomNav = !tableDetailsPattern.test(currentUrl);
+  }
 }
