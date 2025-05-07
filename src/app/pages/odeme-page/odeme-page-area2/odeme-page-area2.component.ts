11e877e9833141c45f18f2d51365dd8fd59d72a9
Furkan KAMACI <furkan.kamaci@amadeus.com>
2025-05-07 15:38:01 +0300

    odeme-page-area2 refactored

--- src/app/pages/odeme-page/odeme-page-area2/odeme-page-area2.component.ts
+++ src/app/pages/odeme-page/odeme-page-area2/odeme-page-area2.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { OdemePageFacadeService } from '../../../services/odeme-page-facade.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-odeme-page-area2',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './odeme-page-area2.component.html',
   styleUrl: './odeme-page-area2.component.scss'
 })
 export class OdemePageArea2Component {
 
+  constructor(public odemePageFacadeService: OdemePageFacadeService) { }
 }
