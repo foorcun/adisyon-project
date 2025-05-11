3db6260703f265419b03b3ef48f94942ca3963ba
Furkan KAMACI <furkan.kamaci@amadeus.com>
2025-05-11 19:18:56 +0300

    para ustu eklendi

--- src/app/pages/odeme-page/odeme-page-area2/odeme-page-area2.component.ts
+++ src/app/pages/odeme-page/odeme-page-area2/odeme-page-area2.component.ts
@@ -1,35 +1,42 @@
 import { Component } from '@angular/core';
-import { OdemePageFacadeService } from '../../../services/odeme-page-facade.service';
 import { CommonModule } from '@angular/common';
+import { Observable, combineLatest, map } from 'rxjs';
+import { OdemePageFacadeService } from '../../../services/odeme-page-facade.service';
 import { OdemeGridComponent } from './odeme-grid/odeme-grid.component';
-import { combineLatest, map, Observable } from 'rxjs';
-import { OrderItem } from '../../../OrderFeature/domain/entities/order-item.entity';
-import { Payment } from '../../../PaymentFeature/domain/entities/payment.entity';
 import { SubpaymentComponent } from './subpayment/subpayment.component';
 
 @Component({
   selector: 'app-odeme-page-area2',
+  standalone: true,
   imports: [CommonModule, OdemeGridComponent, SubpaymentComponent],
   templateUrl: './odeme-page-area2.component.html',
   styleUrl: './odeme-page-area2.component.scss'
 })
 export class OdemePageArea2Component {
 
-
   public totalPrice$: Observable<number>;
   public subPaymentTotal$: Observable<number>;
-
-  public remainingAmount$: Observable<number>;
+  public remainingInfo$: Observable<{ label: string; amount: number }>;
 
   constructor(public odemePageFacadeService: OdemePageFacadeService) {
     this.totalPrice$ = this.odemePageFacadeService.totalPrice$;
     this.subPaymentTotal$ = this.odemePageFacadeService.subPaymentTotal$;
 
-    this.remainingAmount$ = combineLatest([
+    this.remainingInfo$ = combineLatest([
       this.totalPrice$,
       this.subPaymentTotal$
     ]).pipe(
-      map(([total, paid]) => Math.max(total - paid, 0))
+      map(([total, paid]) => {
+        const diff = paid - total;
+
+        if (diff < 0) {
+          return { label: 'Kalan Tutar', amount: Math.abs(diff) };
+        } else if (diff > 0) {
+          return { label: 'Para Üstü', amount: diff };
+        } else {
+          return { label: 'Tam Ödendi', amount: 0 };
+        }
+      })
     );
   }
 }
