6825f87a77aacf2f4829798779796a0cbefba2a8
Furkan KAMACI <furkan.kamaci@amadeus.com>
2025-05-12 11:51:59 +0300

    multiple selected at area1

--- src/app/pages/odeme-page/odeme-page-area1/odeme-page-area1.component.ts
+++ src/app/pages/odeme-page/odeme-page-area1/odeme-page-area1.component.ts
@@ -9,14 +9,22 @@ import { OdemePageFacadeService } from '../../../services/odeme-page-facade.serv
   styleUrl: './odeme-page-area1.component.scss'
 })
 export class OdemePageArea1Component {
+  selectedItems: any[] = [];
 
-  constructor(
-    public odemePageFacadeService: OdemePageFacadeService
-  ) {
+  constructor(public odemePageFacadeService: OdemePageFacadeService) { }
+
+  toggleItemSelection(item: any): void {
+    const index = this.selectedItems.indexOf(item);
+    if (index > -1) {
+      this.selectedItems.splice(index, 1); // remove
+    } else {
+      this.selectedItems.push(item); // add
+    }
+    console.log('Selected Items:', this.selectedItems);
   }
 
-  onItemClick(item: any): void {
-    console.log('Clicked:', item);
+  isSelected(item: any): boolean {
+    return this.selectedItems.includes(item);
   }
-
 }
+
